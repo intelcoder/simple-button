d1887116a29e3fb103f1898b0e17006ac2cae600
Seongjun kim <seongjun@condos.ca>
2017-05-24 13:20:20 -0400

    updated

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
-import React, { PropTypes } from 'react'
+'use strict';
 
-import css from './style.css'
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-const SimpleButton = ({full, style, float, label, onClick}) => {
-  let cssClass = css.simpleButton;
-  if(full) cssClass = cssClass + ' ' + css.full;
-  if(style) cssClass = cssClass + ' ' + css[style];
-  if(float) cssClass = cssClass + ' ' + css[float];
+var _react = require('react');
 
-  return (
-    <button className={ cssClass } onClick={onClick}>{label}</button>
-  )
+var _react2 = _interopRequireDefault(_react);
+
+var _style = require('./style.css');
+
+var _style2 = _interopRequireDefault(_style);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var SimpleButton = function SimpleButton(_ref) {
+  var full = _ref.full,
+      style = _ref.style,
+      float = _ref.float,
+      label = _ref.label,
+      onClick = _ref.onClick;
+
+  var cssClass = _style2.default.simpleButton;
+  if (full) cssClass = cssClass + ' ' + _style2.default.full;
+  if (style) cssClass = cssClass + ' ' + _style2.default[style];
+  if (float) cssClass = cssClass + ' ' + _style2.default[float];
+
+  return _react2.default.createElement(
+    'button',
+    { className: cssClass, onClick: onClick },
+    label
+  );
 };
 
 SimpleButton.propTypes = {
   /**
    * Make button width to 100%
    */
-  full: PropTypes.bool,
-  style: PropTypes.oneOf(['blue', 'lightBlue', 'full','half']),
-  float: PropTypes.oneOf(['floatRight', 'floatLeft']),
-  label: PropTypes.string,
-  onClick: PropTypes.func,
+  full: _react.PropTypes.bool,
+  style: _react.PropTypes.oneOf(['blue', 'lightBlue', 'full', 'half']),
+  float: _react.PropTypes.oneOf(['floatRight', 'floatLeft']),
+  label: _react.PropTypes.string,
+  onClick: _react.PropTypes.func
 };
 
-export default SimpleButton
+exports.default = SimpleButton;
