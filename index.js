a895fb3073d6091c2102cc3c4738bb18abef4241
Seongjun kim <seongjun@condos.ca>
2017-05-24 12:59:30 -0400

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+import React, { PropTypes } from 'react'
+
+import css from './style.css'
+
+const SimpleButton = ({full, style, float, label, onClick}) => {
+  let cssClass = css.simpleButton;
+  if(full) cssClass = cssClass + ' ' + css.full;
+  if(style) cssClass = cssClass + ' ' + css[style];
+  if(float) cssClass = cssClass + ' ' + css[float];
+
+  return (
+    <button className={ cssClass } onClick={onClick}>{label}</button>
+  )
+};
+
+SimpleButton.propTypes = {
+  /**
+   * Make button width to 100%
+   */
+  full: PropTypes.bool,
+  style: PropTypes.oneOf(['blue', 'lightBlue', 'full','half']),
+  float: PropTypes.oneOf(['floatRight', 'floatLeft']),
+  label: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default SimpleButton
